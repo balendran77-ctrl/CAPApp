4ecd50bf00e65b7ad9bb463a1ed9ab363c1f3783
balendran77-ctrl <balendran77@gmail.com>
2026-02-11 09:22:28 -0600

    Update API base URL to production server

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:5000/api';
+  static const String baseUrl = 'https://capapp-hzwm.onrender.com/api';
   static const String tokenKey = 'jwt_token';
 
   static String? _token;
